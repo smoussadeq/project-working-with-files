5dc5d2d9f4a1323c7f752813bc42d4f4bc6fe6ec
Said Moussadeq <saidmoussadeq@saids-MacBook-Pro.local>
2016-04-02 23:42:09 -0400

    3 rd att

--- randomQuizGenerator.py
+++ randomQuizGenerator.py
@@ -3,7 +3,9 @@
 # random order, along with the answer key.
 
 import random, os
-
+os.makedirs(os.path.join('./answers'), exist_ok=True)
+os.makedirs(os.path.join('./quizzes'), exist_ok=True)
+            
 # The quiz data. Keys are states and values are their capitals.
 capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix',
             'Arkansas': 'Little Rock', 'California': 'Sacramento', 'Colorado': 'Denver',
@@ -29,41 +31,30 @@ capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix',
             #       2. instead of creating quiz and answer files in the current working directory, create a folder titled 'quizzes' and another folder titled 'answers'.
             #       3. place the randomly-generated quizzes in the 'quizzes' directory.
             #       4. plaec the corresponding answers in the 'answers' directory.
-
-
-os.makedirs('quizzes')
-
-os.makedirs('answers')
-
-for quizNum in range (5):
-    quizFile = open ( 'quizzes\\capitalsquiz%s.txt' % (quizNum +1), 'w')
-    answerKeyFile = open ('answers\\capitalsquiz_answers%s.txt' % (quizNum +1), 'w')
-
-    quizFile.write ('Name:\n\nDate:\n\nPeriod:\n\n')
+            
+for quizNum in range(5):
+    quizFile = open('./quizzes/capitalsquiz%s.txt' % (quizNum + 1), 'w')
+    answerKeyFile = open('./answers/capitalsquiz_answers%s.txt' % (quizNum + 1), 'w')
+    quizFile.write('Name:\n\nDate:\n\nPeriod:\n\n')
     quizFile.write((' ' * 20) + 'State Capitals Quiz (Form %s)' % (quizNum + 1))
-    quizFile.write ('\n\n')
-
+    quizFile.write('\n\n')
     states = list(capitals.keys())
-    random.shuffle (states)
-
-    for questionNum in range (50):
+    random.shuffle(states)
+    
+    for questionNum in range(50):
         correctAnswer = capitals[states[questionNum]]
         wrongAnswers = list(capitals.values())
         del wrongAnswers[wrongAnswers.index(correctAnswer)]
         wrongAnswers = random.sample(wrongAnswers, 3)
         answerOptions = wrongAnswers + [correctAnswer]
         random.shuffle(answerOptions)
-                
-
-#for questionNum in range(50):
-            
-        quizFile.write('%s. What is the capital of %s?\n' % (questionNum + 1,states[questionNum]))
-        
+    
+        quizFile.write('%s. What is the capital of %s\n' % (questionNum + 1, states[questionNum]))
         for i in range(4):
-            quizFile.write(' %s. %s\n' % ('ABCD'[i], answerOptions[i]))
-        quizFile.write('\n')
+            quizFile.write('%s. %s\n' % ('ABCD'[i], answerOptions[i]))
+            quizFile.write('\n')
     
-        answerKeyFile.write('%s. %s\n' % (questionNum + 1, 'ABCD'[answerOptions.index(correctAnswer)]))
-    
+        answerKeyFile.write('%s. %s\n' % (questionNum + 1, 'ABCD' [answerOptions.index(correctAnswer)]))
+        
 quizFile.close()
 answerKeyFile.close()
